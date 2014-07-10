9e7fd1b21739777aac96159af1b5209474d0fbe5
Naoki Takezoe <naoki.takezoe@bizreach.co.jp>
2014-07-10 11:48:45 +0900

    Remove unused annotations

--- src/main/scala/jp/co/bizreach/dynamodb4s/Annotations.scala
+++ src/main/scala/jp/co/bizreach/dynamodb4s/Annotations.scala
@@ -4,5 +4,6 @@ import scala.annotation.StaticAnnotation
 
 final case class hashPk() extends StaticAnnotation
 final case class rangePk() extends StaticAnnotation
-final case class name(name: String) extends StaticAnnotation
-final case class version() extends StaticAnnotation
+
+//final case class name(name: String) extends StaticAnnotation
+//final case class version() extends StaticAnnotation
