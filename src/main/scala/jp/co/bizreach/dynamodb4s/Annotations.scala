8b633db15caf3ca921ea71c9db44f2005871803c
Naoki Takezoe <naoki.takezoe@bizreach.co.jp>
2014-07-28 08:21:44 +0900

    Remove Annotations.scala

--- src/main/scala/jp/co/bizreach/dynamodb4s/Annotations.scala
+++
@@ -1,9 +0,0 @@
-package jp.co.bizreach.dynamodb4s
-
-import scala.annotation.StaticAnnotation
-
-final case class hashPk() extends StaticAnnotation
-final case class rangePk() extends StaticAnnotation
-
-//final case class name(name: String) extends StaticAnnotation
-//final case class version() extends StaticAnnotation
